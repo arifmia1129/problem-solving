6b477c433dafb65ef77fb879ba97272b2e072168
Arif <arifmia1129@gmail.com>
2025-10-08 07:23:35 +0600

    J - R2

---
+++ week1/j_r2.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int r1, s; cin >> r1 >> s;
+
+    cout << (s * 2) - r1;
+    
+    return 0;
+}
