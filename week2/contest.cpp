e703ae497ddfeb61b4898356f24f41f13d4d972b
Arif <arifmia1129@gmail.com>
2025-10-16 15:56:42 +0600

    D - Misha and Changing Handles

---
+++ week2/contest.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t; cin >> t;
+
+    while(t--) {
+        int n; cin >> n;
+
+        if(n%2 == 0) {
+            cout << n/2 << endl;
+        }else{
+            cout << (n - 1)/2 + 1 << endl;
+        }
+    }
+    
+    return 0;
+}
