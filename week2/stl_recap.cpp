74c002821ce664b3f1d433425261d97634e6420a
Arif <arifmia1129@gmail.com>
2025-11-23 07:51:32 +0600

    STL pair and tuple recap

---
+++ week2/stl_recap.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+int main () {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    // pair<string, int> student = {"ariba", 1};
+
+    // cout << student.first << " " << student.second << "\n";
+
+    // student.first = "arif";
+    // student.second = 2;
+
+    // auto [name, roll] = student;
+
+    // cout << name << " " << roll << endl;
+
+    // int n; cin >> n;
+
+    // pair<string, int> students[n];
+
+
+    // for(int i = 0; i < n; i++){
+    //     string name; cin >> name;
+    //     int roll; cin >> roll;
+
+    //     students[i].first = name;
+    //     students[i].second = roll;
+    // }
+
+    // for(auto[x, y]:students) {
+    //     cout << x << " " << y << endl;
+    // }
+
+    // int n; cin >> n;
+
+
+    // tuple<string, int, string> persons[n];
+
+    // for(int i = 0; i < n; i++) {
+    //     string name; cin >> name;
+    //     int roll; cin >> roll;
+    //     string gender; cin >> gender;
+
+    //     persons[i] = {name, roll, gender};
+    // }
+
+    // for(auto[x, y, z]:persons) {
+    //     cout << x << " " << y << " " << z << "\n";
+    // }
+
+    
+    return 0;
+}
